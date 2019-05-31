2ff0d40bd56c96926b8b23daff2dc416b29c88dc
Padeen <paulo.deen@gmail.com>
2019-05-31 23:03:06 +0200

    Add portfolio edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,8 +3,15 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def show
+  end
+
   def new
     @portfolio_item = Portfolio.new
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def create
@@ -15,6 +22,20 @@ class PortfoliosController < ApplicationController
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
