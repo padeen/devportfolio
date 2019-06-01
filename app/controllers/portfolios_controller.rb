0af71414ee2993c16cc4a77b7eb431f038bd530b
Padeen <paulo.deen@gmail.com>
2019-06-01 02:41:15 +0200

    Add portfolios destroy

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,4 +40,12 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully destroyed.' }
+    end
+  end
 end
