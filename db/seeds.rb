6f1aebfcbc0fa5c2e7e647ffb24e8fe36405dd27
Padeen <paulo.deen@gmail.com>
2019-06-02 02:18:57 +0200

    Add custom scope to portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Some lipsum here"
+    body: "Some lipsum here",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Some more lorem ipsum generated",
+    main_image: "http://placehold.it/300x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Some more lorem ipsum generated",
     main_image: "http://placehold.it/300x400",
     thumb_image: "http://placehold.it/350x200"
