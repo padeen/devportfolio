54d1dec2f6b86274d714a846fe56b549922d9d8e
Padeen <paulo.deen@gmail.com>
2019-06-04 23:45:43 +0200

    Refactor strong params for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,9 +21,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
-                                                                     technologies_attributes: [
-                                                                     :name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -38,7 +36,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
 
 
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
@@ -55,4 +53,13 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully destroyed.' }
     end
   end
+
+  private
+    def portfolio_params
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle, 
+                                        :body, 
+                                        technologies_attributes: [:name]
+                                       )
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module DevPortfolio
   class Application < Rails::Application
+    # config.action_controller.permit_all_parameters = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
