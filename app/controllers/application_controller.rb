4b6fddfe906395ff016a6d370f6f65d0718a43e0
Padeen <paulo.deen@gmail.com>
2019-06-04 22:54:01 +0200

    Add concern for passing data between pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
+  include SetSource
   protect_from_forgery with: :exception
   before_action :set_source
 
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
 end
