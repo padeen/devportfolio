877db1e891ea5360c5b9e30775794da6ab3bf1c9
Padeen <paulo.deen@gmail.com>
2019-06-05 00:30:06 +0200

    Add guest user Add current user concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-  protect_from_forgery with: :exception
-  before_action :set_source
+  include CurrentUserConcern
 
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    OpenStruct.new(name: 'Guest User',
+      first_name: 'Guest',
+      last_name: 'User',
+      email: 'guest@example.com'
+    )
+  end
 end
