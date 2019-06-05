7e23610c875abad14311af3f637d09217b8317ee
Padeen <paulo.deen@gmail.com>
2019-06-05 03:26:01 +0200

    Add default page content concern Add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,6 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContent
 
-  def current_user
-    super || guest_user
-  end
-
-  def guest_user
-    OpenStruct.new(name: 'Guest User',
-      first_name: 'Guest',
-      last_name: 'User',
-      email: 'guest@example.com'
-    )
-  end
 end
