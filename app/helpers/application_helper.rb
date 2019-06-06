da16efcb95966cdd79d6116e052f7015256a78df
Padeen <paulo.deen@gmail.com>
2019-06-06 18:15:43 +0200

    Add custom gem for copyright banner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting')
     end
   end
+
+  def copyright_generator
+    PadeenViewTool::Renderer.copyright 'Pa Deen', 'All rights reserved'
+  end
 end
