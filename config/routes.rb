5904643c1e8f6760bce822c53c94c17cc8ea5e84
Padeen <paulo.deen@gmail.com>
2019-06-18 18:14:19 +0200

    Create comment broadcast, route and callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   resources :blogs do
     member do
       get :toggle_status
@@ -10,10 +10,14 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
+
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to:'pages#about'
   get 'contact', to:'pages#contact'
+
+  mount ActionCable.server => '/cable'
+
   root to: 'pages#home'
 end
